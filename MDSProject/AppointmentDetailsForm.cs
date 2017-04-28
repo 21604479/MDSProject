2b18eb38030d15da63c4756fcbfd5c5ef9601251
alexa <alexa@DESKTOP-JH6OCVP>
2017-04-28 10:09:59 +0100

    Change Appointment detail

--- MDSProject/AppointmentDetailsForm.cs
+++ MDSProject/AppointmentDetailsForm.cs
@@ -25,7 +25,7 @@ namespace MDSProject
         private void buttonCancelAppointment_Click(object sender, EventArgs e)
         {
 
-            if (true)
+            if ()
             {
                 Close();
                 ErrorMessageForm errorMessage = new ErrorMessageForm();

18c316e55fa770440075a17ade70d1f9636ffaa4
LHCCM <2160435@my.ipleiria.pt>
2017-06-02 17:07:17 +0100

    caixa apagada

--- MDSProject/CancelForm.Designer.cs
+++ MDSProject/CancelForm.Designer.cs
@@ -34,16 +34,15 @@
             this.buttonCancel = new System.Windows.Forms.Button();
             this.textBoxSearch = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelHour = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelDoctorName = new System.Windows.Forms.Label();
+            this.labelPacientName = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.labelPacientName = new System.Windows.Forms.Label();
-            this.labelDoctorName = new System.Windows.Forms.Label();
-            this.labelDate = new System.Windows.Forms.Label();
-            this.labelHour = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,28 +50,28 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(248, 9);
+            this.label1.Location = new System.Drawing.Point(186, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 25);
+            this.label1.Size = new System.Drawing.Size(108, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Appointments";
             // 
             // listBoxAppointment
             // 
             this.listBoxAppointment.FormattingEnabled = true;
-            this.listBoxAppointment.ItemHeight = 16;
-            this.listBoxAppointment.Location = new System.Drawing.Point(12, 37);
-            this.listBoxAppointment.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listBoxAppointment.Location = new System.Drawing.Point(9, 30);
+            this.listBoxAppointment.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBoxAppointment.Name = "listBoxAppointment";
-            this.listBoxAppointment.Size = new System.Drawing.Size(249, 260);
+            this.listBoxAppointment.Size = new System.Drawing.Size(188, 212);
             this.listBoxAppointment.TabIndex = 1;
             // 
             // buttonSearch
             // 
-            this.buttonSearch.Location = new System.Drawing.Point(12, 371);
-            this.buttonSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonSearch.Location = new System.Drawing.Point(9, 301);
+            this.buttonSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonSearch.Name = "buttonSearch";
-            this.buttonSearch.Size = new System.Drawing.Size(90, 28);
+            this.buttonSearch.Size = new System.Drawing.Size(68, 23);
             this.buttonSearch.TabIndex = 2;
             this.buttonSearch.Text = "Search";
             this.buttonSearch.UseVisualStyleBackColor = true;
@@ -81,10 +80,10 @@
             // buttonCancel
             // 
             this.buttonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.buttonCancel.Location = new System.Drawing.Point(171, 370);
-            this.buttonCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonCancel.Location = new System.Drawing.Point(128, 301);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(90, 28);
+            this.buttonCancel.Size = new System.Drawing.Size(68, 23);
             this.buttonCancel.TabIndex = 3;
             this.buttonCancel.Text = "Select";
             this.buttonCancel.UseVisualStyleBackColor = true;
@@ -92,16 +91,15 @@
             // 
             // textBoxSearch
             // 
-            this.textBoxSearch.Location = new System.Drawing.Point(12, 319);
-            this.textBoxSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxSearch.Location = new System.Drawing.Point(9, 259);
+            this.textBoxSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxSearch.Name = "textBoxSearch";
-            this.textBoxSearch.Size = new System.Drawing.Size(249, 22);
+            this.textBoxSearch.Size = new System.Drawing.Size(188, 20);
             this.textBoxSearch.TabIndex = 5;
             this.textBoxSearch.Text = "Search";
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.labelHour);
             this.groupBox1.Controls.Add(this.labelDate);
             this.groupBox1.Controls.Add(this.labelDoctorName);
@@ -110,103 +108,107 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Location = new System.Drawing.Point(267, 37);
+            this.groupBox1.Location = new System.Drawing.Point(200, 30);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(399, 260);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(299, 211);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Details";
             // 
+            // labelHour
+            // 
+            this.labelHour.AutoSize = true;
+            this.labelHour.Location = new System.Drawing.Point(89, 186);
+            this.labelHour.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelHour.Name = "labelHour";
+            this.labelHour.Size = new System.Drawing.Size(0, 13);
+            this.labelHour.TabIndex = 11;
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Location = new System.Drawing.Point(89, 134);
+            this.labelDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(0, 13);
+            this.labelDate.TabIndex = 10;
+            // 
+            // labelDoctorName
+            // 
+            this.labelDoctorName.AutoSize = true;
+            this.labelDoctorName.Location = new System.Drawing.Point(89, 82);
+            this.labelDoctorName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelDoctorName.Name = "labelDoctorName";
+            this.labelDoctorName.Size = new System.Drawing.Size(0, 13);
+            this.labelDoctorName.TabIndex = 9;
+            // 
+            // labelPacientName
+            // 
+            this.labelPacientName.AutoSize = true;
+            this.labelPacientName.Location = new System.Drawing.Point(89, 31);
+            this.labelPacientName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelPacientName.Name = "labelPacientName";
+            this.labelPacientName.Size = new System.Drawing.Size(0, 13);
+            this.labelPacientName.TabIndex = 8;
+            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 229);
+            this.label4.Location = new System.Drawing.Point(10, 186);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(43, 17);
+            this.label4.Size = new System.Drawing.Size(33, 13);
             this.label4.TabIndex = 7;
             this.label4.Text = "Hour:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 165);
+            this.label3.Location = new System.Drawing.Point(10, 134);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 17);
+            this.label3.Size = new System.Drawing.Size(33, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Date:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 101);
+            this.label2.Location = new System.Drawing.Point(10, 82);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.Size = new System.Drawing.Size(73, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "Doctor Name:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(13, 38);
+            this.label5.Location = new System.Drawing.Point(10, 31);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(100, 17);
+            this.label5.Size = new System.Drawing.Size(77, 13);
             this.label5.TabIndex = 4;
             this.label5.Text = "Pacient Name:";
             // 
-            // labelPacientName
-            // 
-            this.labelPacientName.AutoSize = true;
-            this.labelPacientName.Location = new System.Drawing.Point(119, 38);
-            this.labelPacientName.Name = "labelPacientName";
-            this.labelPacientName.Size = new System.Drawing.Size(0, 17);
-            this.labelPacientName.TabIndex = 8;
-            // 
-            // labelDoctorName
-            // 
-            this.labelDoctorName.AutoSize = true;
-            this.labelDoctorName.Location = new System.Drawing.Point(119, 101);
-            this.labelDoctorName.Name = "labelDoctorName";
-            this.labelDoctorName.Size = new System.Drawing.Size(0, 17);
-            this.labelDoctorName.TabIndex = 9;
-            // 
-            // labelDate
-            // 
-            this.labelDate.AutoSize = true;
-            this.labelDate.Location = new System.Drawing.Point(119, 165);
-            this.labelDate.Name = "labelDate";
-            this.labelDate.Size = new System.Drawing.Size(0, 17);
-            this.labelDate.TabIndex = 10;
-            // 
-            // labelHour
-            // 
-            this.labelHour.AutoSize = true;
-            this.labelHour.Location = new System.Drawing.Point(119, 229);
-            this.labelHour.Name = "labelHour";
-            this.labelHour.Size = new System.Drawing.Size(0, 17);
-            this.labelHour.TabIndex = 11;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(389, 338);
+            this.button1.Location = new System.Drawing.Point(292, 275);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 37);
+            this.button1.Size = new System.Drawing.Size(120, 30);
             this.button1.TabIndex = 10;
             this.button1.Text = "Cancel Appointment";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(216, 69);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(141, 22);
-            this.textBox1.TabIndex = 12;
-            // 
             // CancelForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(678, 410);
+            this.ClientSize = new System.Drawing.Size(508, 333);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.textBoxSearch);
@@ -214,7 +216,7 @@
             this.Controls.Add(this.buttonSearch);
             this.Controls.Add(this.listBoxAppointment);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "CancelForm";
             this.Text = "Cancel Appointment";
             this.groupBox1.ResumeLayout(false);
@@ -241,6 +243,5 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
